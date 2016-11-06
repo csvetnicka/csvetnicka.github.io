0f64ca730e843f85607d3fa38069210f48f9a84b
Kionte Harris <kmharri@umich.edu>
2016-11-06 03:34:59 -0500

    Recommendations Implemented
    It may look a little complex (messy*) with the logic. I threw some comments in that will hopefully make it easier for you to follow along. Please test and see and any errors occur.

--- app.js
+++ app.js
@@ -18,8 +18,49 @@ var courses = {"EECS": ["183",   //List of courses in EECS department?
 "487",
 "490", "493","497"],
 "ENGR": ["101"] };
-
 courses["EECS"].sort();
+
+//Course Prerequisite Dictionary
+//course_Prereqs['course name'][0] will always be the course's title
+//"|" allows for differentiation: [(Course Name), All Required,..., | , One Of,..., |, No Credit,...]
+var course_Prereqs = { "ENGR 101" : ["Intro Comp&Prog", "|", "|", "ENGR 151", "EECS 183"],
+					   "EECS 183" : ["Elementary Programming Concepts", "|", "|", "ENGR 101", "ENGR 151"], 
+					   "EECS 203" : ["Discrete Mathematics", "|", "MATH 115", "MATH 116", "MATH 119", "MATH 120", "MATH 121", "MATH 156", "MATH 175",
+					   				 "MATH 176", "MATH 185", "MATH 186", "MATH 214", "MATH 215", "MATH 216", "MATH 217", "MATH 255", "MATH 256",
+					   				 "MATH 285", "MATH 286", "MATH 295", "MATH 296", "MATH 417", "MATH 419"],
+					   "EECS 270" : ["Introduction to Logic Design", "|", "EECS 183", "ENGR 101"],
+					   "EECS 280" : ["Programming and Introductory Data Structures", "|", "EECS 182", "EECS 183", "ENGR 101", "ENGR 151"],
+					   "EECS 281" : ["Data Structures and Algorithms", "EECS 280", "|", "EECS 203", "MATH 465", "MATH 565"],
+					   "EECS 370" : ["Introduction to Computer Organization", "EECS 280", "|", "EECS 203", "EECS 270", "MATH 465", "MATH 565"],
+					   "EECS 373" : ["Design of Microprocessor Based Systems", "EECS 270", "EECS 370"],
+					   "EECS 376" : ["Foundations of Computer Science", "EECS 280", "|", "EECS 203", "MATH 465", "MATH 565"],
+					   "EECS 381" : ["Object Orientated and Advanced Programming", "EECS 281", "EECS 370"],
+					   "EECS 382" : ["Internet-scale Computing", "|", "EECS 281", "EECS 282"],
+					   "EECS 388" : ["Introduction to Computer Security", "EECS 281"],
+					   "EECS 427" : ["VLSI Design I", "EECS 270", "EECS 312", "EECS 320"],
+					   "EECS 441" : ["Mobile App Development for Entrepreneurs", "EECS 281", "EECS 370", "|", "EECS 373", "EECS 381", "EECS 388",
+					   				 "EECS 427", "EECS 442", "EECS 445", "EECS 467", "EECS 470", "EECS 475", "EECS 477", "EECS 478", "EECS 482", 
+					   				 "EECS 483", "EECS 484", "EECS 485", "EECS 486", "EECS 487", "EECS 489", "EECS 490", "EECS 492", "EECS 493"],
+					   "EECS 442" : ["Computer Vision", "EECS 281"],
+					   "EECS 445" : ["Introduction to Machine Learning", "EECS 281"],
+					   "EECS 467" : ["Autonomous Robotics", "EECS 281"],
+					   "EECS 470" : ["Computer Architecture", "EECS 270", "EECS 370"],
+					   "EECS 475" : ["Introduction to Cryptography", "EECS 280", "EECS 376", "|", "EECS 203", "MATH 312", "MATH 412"],
+					   "EECS 477" : ["Introduction to Algorithms", "EECS 281", "EECS 376"],
+					   "EECS 481" : ["Software Engineering", "EECS 281"],
+					   "EECS 482" : ["Introduction to Operating Systems", "EECS 281", "EECS 370"],
+					   "EECS 483" : ["Compiler Construction", "EECS 281"],
+					   "EECS 484" : ["Database Management Systems", "EECS 281"],
+					   "EECS 485" : ["Web Database and Information Systems", "|", "EECS 281", "EECS 382"],
+					   "EECS 486" : ["Information Retrieval and Web Search", "EECS 281"],
+					   "EECS 487" : ["Interactive Computer Graphics", "EECS 281"],
+					   "EECS 489" : ["Computer Networks", "EECS 482"],
+					   "EECS 490" : ["Programming Languages", "EECS 281"],
+					   "EECS 492" : ["Introduction to Artificial Intelligence", "EECS 281"],
+					   "EECS 493" : ["User Interface Development", "EECS 281"],
+					   "EECS 494" : ["Computer Game Design and Development", "EECS 281"]
+					   };
+
 app.use(morgan("short"));
 var publicPath = path.join(__dirname,"Public"); //Joins current working directory with Public
 
@@ -74,7 +115,85 @@ app.post("/createaccount",function(req,res){
 
 app.post("/createsuggestions",function(req,res){
 	console.log("Post request received");
-	console.log(req.body.course0);
+
+	//Store Courses user has taken (For comparison)
+	var takenKeys = Object.keys(req.body);
+	Taken = []
+
+	for (var i = 0; i < req.body.numCourses; i++) {
+		Taken.push(req.body[takenKeys[i]].toUpperCase());
+	}
+	
+	console.log("COURSES TAKEN: " + Taken);
+	//Compare taken courses to prereqs and return recommendations
+	var recommendations =[]
+	var key = Object.keys(course_Prereqs);
+	var size = key.length;
+	
+	for (var i = 0; i < size; i++) {
+		var check = course_Prereqs[key[i]];
+		var j = 1;
+
+		if (Taken.indexOf(key[i]) == -1) { //User has not taken this course already
+			console.log(key[i]);
+			console.log(check);
+			//All Required
+			var canTake = true;
+			while((j < check.length) && (check[j] != '|')) {
+				if (Taken.indexOf(check[j]) == -1) {
+					canTake = false;
+					break; //No need to check further
+				}
+				j++;
+			}
+			if (canTake == false) {
+				continue; //User has not taken all required courses
+			}
+			console.log("Passed canTake");
+
+			//One of
+			j++;
+			console.log(j);
+			var oneOf = false;
+			if (((j < check.length) && (check[j] == '|')) || (j >= check.length)) {
+				oneOf = true;
+			} else {
+				while((j < check.length) && (check[j] != '|')) {
+					if (Taken.indexOf(check[j]) != -1) {
+						oneOf = true;
+					}
+					j++;
+				}
+			}
+			if (oneOf == false) {
+				continue; //User has not taken at least one of required "OR" courses
+			}
+			console.log("Passed One of");
+
+			//No Credit will be given
+			j++;
+			console.log(j);
+			console.log(check.length);
+			var noCredit = false;
+			while(j < check.length) {
+				if (Taken.indexOf(check[j]) != -1) {
+					console.log(check[j]);
+					noCredit = true;
+					break; //No need to check further
+				}
+				j++;
+			}
+			if (noCredit == true) {
+				continue; //User will not be given credit for this course
+			}
+			console.log("Passed No credit");
+
+			//Add This course to reccomendations for user
+			recommendations.push(key[i] + ": " + course_Prereqs[key[i]][0]);
+		}
+	}
+	console.log(recommendations);
+
 });
 
 
