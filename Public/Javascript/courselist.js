42323b4f73fc474eadba46e4b281ca075f71ec95
ksandera <ksandera@umich.edu>
2016-11-26 05:38:37 -0500

    Able to see new comments entered without refreshing

--- Public/Javascript/courselist.js
+++ Public/Javascript/courselist.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
 	console.log("This document is in fact ready");
 	$("form").submit(function(e){
 		e.preventDefault();
+		var comment = $("#comment-text").val();
 		//console.log($("#comment-text").val());
 		console.log($("#course-name").text())
 		event.preventDefault();
 		$.ajax({url: "http://localhost:3000/courseguide/addcomment", type: "post",
-		data: {"comment": $("#comment-text").val(), "time": Date.getTime, 
+		data: {"comment": comment, "time": Date.getTime, 
 		course: $("#course-name").text()}, success:
 		function(data,status,xhr){
 			console.log("Completed");
+			if(status === "success"){
+				$("#comment-body").append("<tr> <td>" + comment + "</td> </tr>");
+			}
 		}});
 	return false;
 	});
